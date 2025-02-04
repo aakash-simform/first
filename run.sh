6fda05efa8d38e51f2f198231ae01964bb4c2b02
aakash.shah <aakash.shah@simformsolutions.com>
2025-02-04 17:54:02 +0530

    Committed on Tuesday 04 February 2025 05:54:02 PM IST

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DATE=$(date)
 echo $DATE >> /home/aakash.shah@simform.dom/Desktop/github/crondemo/gittry/data.txt
+cd /home/aakash.shah@simform.dom/Desktop/github/crondemo/gittry
 git add .
 git commit -m "Committed on $DATE"
 git push
